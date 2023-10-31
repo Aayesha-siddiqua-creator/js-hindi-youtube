1551bc74ade70b1082551cb0b5158fc5ca2e0fd8
Aayesha-siddiqua-creator <147506034+Aayesha-siddiqua-creator@users.noreply.github.com>
2023-10-31 10:11:24 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = Aurangabad
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console. log(accountId);
+
+console, table([laccountId, accountEmail, accountPassword, accountCity])
